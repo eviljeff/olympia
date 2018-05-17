6f24e4f8d63b048407d7bfb06a7ed9dc2ce2070c
Andrew Williamson <awilliamson@mozilla.com>
2018-05-17 14:52:41 +0100

    match both v3 and v4 when we're testing for api in middleware, etc

--- src/olympia/api/urls.py
+++ src/olympia/api/urls.py
@@ -12,8 +12,9 @@ v3_api_urls = [
     url(r'^activity/', include('olympia.activity.urls')),
     url(r'^github/', include('olympia.github.urls')),
 ]
+v4_api_urls = list(v3_api_urls)
 
 urlpatterns = [
     url(r'^v3/', include(v3_api_urls, namespace='v3')),
-    url(r'^v4/', include(v3_api_urls, namespace='v4')),
+    url(r'^v4/', include(v4_api_urls, namespace='v4')),
 ]
